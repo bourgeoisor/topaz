c870fb5031d10bc4389b16cf55f3f81ad7a01664
bourgeoisor <3271352+bourgeoisor@users.noreply.github.com>
2018-12-22 13:47:13 -0400

    Initial game code

---
+++ Topaz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Topaz
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var topaz = new Topaz())
+                topaz.Run();
+        }
+    }
+}
