127ab4bbe86cf0379b09fd14b8ab6cb9c963acea
bourgeoisor <3271352+bourgeoisor@users.noreply.github.com>
2018-12-25 15:05:35 -0400

    Added stacktrace on exception

--- Topaz/Program.cs
+++ Topaz/Program.cs
@@ -2,19 +2,26 @@
 
 namespace Topaz
 {
-    /// <summary>
-    /// The main class.
-    /// </summary>
     public static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            using (var topaz = new Topaz())
-                topaz.Run();
+            #if DEBUG
+                using (var topaz = new Topaz())
+                    topaz.Run();
+            #else
+                try
+                {
+                    using (var topaz = new Topaz())
+                        topaz.Run();
+                }
+                catch (Exception e)
+                {
+                    Engine.Window.Instance.Terminate();
+                    System.Windows.Forms.MessageBox.Show(e.ToString(), "Runtime Error");
+                }
+            #endif
         }
     }
 }
