735949750642decda10deeac08831992e07e5aa4
bourgeoisor <3271352+bourgeoisor@users.noreply.github.com>
2019-01-16 00:01:49 -0400

    Add serializable settings model

--- Topaz/Source/Engine/Window.cs
+++ Topaz/Source/Engine/Window.cs
@@ -7,13 +7,19 @@ namespace Topaz.Engine
 {
     public sealed class Window
     {
-        const int DEFAULT_WINDOW_WIDTH = 1280;
-        const int DEFAULT_WINDOW_HEIGHT = 800;
+        const string SETTINGS_FILE_PATH = "settings.txt";
 
-        public GraphicsDeviceManager Graphics { get; private set; }
-        public GraphicsDevice GraphicsDevice { get; private set; }
+        private WindowState _state;
+        private Settings _settings;
+        private Game _game;
+        private GraphicsDeviceManager _graphics;
+        private GraphicsDevice _graphicsDevice;
 
-        public WindowState State { get; set; }
+        public WindowState State { get => _state; set => _state = value; }
+        public Settings Settings { get => _settings; set => _settings = value; }
+        public Game Game { get => _game; set => _game = value; }
+        public GraphicsDeviceManager Graphics { get => _graphics; private set => _graphics = value; }
+        public GraphicsDevice GraphicsDevice { get => _graphicsDevice; private set => _graphicsDevice = value; }
 
         public enum WindowState { Running, Terminating }
 
@@ -24,29 +30,32 @@ namespace Topaz.Engine
 
         private Window()
         {
-            State = WindowState.Running;
+            _state = WindowState.Running;
+
+            _settings = Engine.Util.XmlSerialization.ReadFromXmlFile<Engine.Settings>(SETTINGS_FILE_PATH);
+            if (_settings == null)
+                _settings = new Engine.Settings();
+
+            Engine.Util.XmlSerialization.WriteToXmlFile<Engine.Settings>(SETTINGS_FILE_PATH, _settings);
         }
 
         public void Initialize(Game game, GraphicsDeviceManager graphics)
         {
-            this.Graphics = graphics;
-            this.GraphicsDevice = game.GraphicsDevice;
+            _game = game;
+            _graphics = graphics;
+            _graphicsDevice = game.GraphicsDevice;
 
             string title = Properties.Resources.Title;
             if (Properties.Resources.DevMode == "true")
-            {
                 title += " " + Properties.Resources.Version + "-" + Properties.Resources.GitCount + "-" + Properties.Resources.GitHash;
-            }
 
-            game.Window.Title = title;
-            game.Window.AllowUserResizing = true;
-            game.IsMouseVisible = true;
-            game.IsFixedTimeStep = true;
+            _game.Window.Title = title;
+            _game.Window.AllowUserResizing = true;
+            _game.IsMouseVisible = true;
+            _game.IsFixedTimeStep = _settings.Video.Vsync;
+            _graphics.SynchronizeWithVerticalRetrace = _settings.Video.Vsync;
 
-            graphics.SynchronizeWithVerticalRetrace = true;
-            graphics.PreferredBackBufferWidth = DEFAULT_WINDOW_WIDTH;
-            graphics.PreferredBackBufferHeight = DEFAULT_WINDOW_HEIGHT;
-            graphics.ApplyChanges();
+            ToggleFullscreen(_settings.Video.Fullscreen);
 
             Engine.Content.Instance.Initialize(game, graphics);
             Scene.SceneManager.Instance.Initialize();
@@ -70,21 +79,7 @@ namespace Topaz.Engine
                 Terminate();
 
             if (Engine.Input.Instance.IsKeyDown(Keys.LeftControl) && Engine.Input.Instance.IsKeyDown(Keys.Enter))
-            {
-                if (Graphics.IsFullScreen)
-                {
-                    Graphics.PreferredBackBufferWidth = DEFAULT_WINDOW_WIDTH;
-                    Graphics.PreferredBackBufferHeight = DEFAULT_WINDOW_HEIGHT;
-                }
-                else
-                {
-                    Graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
-                    Graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
-                }
-
-                Graphics.IsFullScreen = !Graphics.IsFullScreen;
-                Graphics.ApplyChanges();
-            }
+                ToggleFullscreen(!_graphics.IsFullScreen);
 
             Scene.SceneManager.Instance.Update(gameTime);
             Input.Instance.Update(gameTime);
@@ -97,14 +92,45 @@ namespace Topaz.Engine
 
         public void Terminate()
         {
-            State = WindowState.Terminating;
+            _state = WindowState.Terminating;
+
             Networking.Client.Instance.Disconnect();
             Networking.Server.Instance.Terminate();
+
+            SaveSettings();
         }
 
+        public void SaveSettings()
+        {
+            _settings.Video.Fullscreen = _graphics.IsFullScreen;
+            if (!_graphics.IsFullScreen)
+            {
+                _settings.Video.WindowedScreenWidth = GetViewport().Width;
+                _settings.Video.WindowedScreenHeight = GetViewport().Height;
+            }
+            Engine.Util.XmlSerialization.WriteToXmlFile<Engine.Settings>(SETTINGS_FILE_PATH, _settings);
+        }
+        
         public Viewport GetViewport()
         {
-            return GraphicsDevice.Viewport;
+            return _graphicsDevice.Viewport;
+        }
+
+        public void ToggleFullscreen(bool toggle)
+        {
+            if (toggle)
+            {
+                _graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
+                _graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
+            }
+            else
+            {
+                _graphics.PreferredBackBufferWidth = _settings.Video.WindowedScreenWidth;
+                _graphics.PreferredBackBufferHeight = _settings.Video.WindowedScreenHeight;
+            }
+
+            _graphics.IsFullScreen = toggle;
+            _graphics.ApplyChanges();
         }
     }
 }
